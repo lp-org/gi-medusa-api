d0e17a96029c0bb044f047203e1301d07d31fbda
Lizarddapp <Lizarddapp@gmail.com>
2023-09-23 17:06:06 +0800

    update type

--- src/api/routes/store/weight-fulfillment.ts
+++ src/api/routes/store/weight-fulfillment.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import WeightFulfillmentRepository from "../../../repositories/WeightFulfillment";
+import WeightFulfillmentRepository from "../../../repositories/weightFulfillment";
 const router = Router();
 router.get("/", async (req, res) => {
   const store = await WeightFulfillmentRepository.find();
